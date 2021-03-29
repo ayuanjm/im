cd6fe801efc891e6d6c398fa505a11def81889f6
ayuanjm <2459087172@qq.com>
2021-03-29 16:48:15 +0800

    im init

---
+++ im-demo/src/main/java/com/yuan/im/imdemo/ImDemoApplication.java
@@ -0,0 +1,16 @@
+package com.yuan.im.imdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yuan
+ */
+@SpringBootApplication
+public class ImDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImDemoApplication.class, args);
+    }
+
+}
---
+++ im-demo/src/test/java/com/yuan/im/imdemo/ImDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuan.im.imdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
